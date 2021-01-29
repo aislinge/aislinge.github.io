06687909fee8090c655a8cc9efcefb0600b1e8db
aislinge <52643744+aislinge@users.noreply.github.com>
2021-01-29 16:24:24 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="main-bg colorful"></div>
+    <div class="main-bg"></div>
     <vue-baberrage
       :isShow="barrageIsShow"
       :barrageList="barrageList"
@@ -13,12 +13,8 @@
         <span
           class="badge rounded-pill"
           :class="slotProps.item.color"
-          style="
-            background: none;
-            color: rgba(0, 0, 0, 0.4);
-            font-family: 'NFM', 'SCM';
-            font-size: 14px;
-          "
+          style="background: none; font-family: 'YRDM'"
+          :style="slotProps.item.size + slotProps.item.color"
         >
           {{ slotProps.item.msg }}
         </span>
@@ -55,7 +51,7 @@ export default {
       now_time,
       msg: "",
       days: now_time.diff(start_time, "days"),
-      throttleGap: 4000,
+      throttleGap: 2000,
       lanesCount: 16,
       messageHeight: 35,
       barrageIsShow: true,
@@ -63,37 +59,21 @@ export default {
       barrageLoop: true,
       // TODO 这里的弹幕内容应该为日记内容摘选
       barrageList: [],
-      // NOTE 为变换颜色创建的数组
-      colorList: [
-        "red",
-        "orange",
-        "yellow",
-        "olive",
-        "green",
-        "teal",
-        "blue",
-        "violet",
-        "purple",
-        "pink",
-      ],
     };
   },
   methods: {
     addToList() {
       axios.get(url).then((response) => {
         for (let index = 0; index < 15; index++) {
+          let tmp_size = Math.random();
           this.barrageList.push({
             id: ++this.currentId,
-            // msg: response.data['hitokoto'],
             msg: response.data["returnObj"][index],
-            time: Math.random() * 10 + 5,
-            // NOTE 这里可以实现变化颜色
-            // color: this.colorList[index % 10],
-            color: "",
+            time: Math.random() * 10 + 10,
+            color:
+              "color: rgba(" + 0 + "," + 0 + "," + 0 + "," + tmp_size + ");",
+            size: "font-size: " + Math.ceil(tmp_size * 2.5) + 2 + "px;",
             type: MESSAGE_TYPE.NORMAL,
-            // username: response.data['from_who']
-            //   ? response.data['from_who']
-            //   : 'Hitokoto',
           });
         }
       });
@@ -110,14 +90,6 @@ export default {
 </script>
 
 <style scoped>
-.main-bg {
-  position: absolute;
-  top: 0;
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
-  z-index: -99999;
-}
 .main {
   padding-top: 150px;
   text-align: center;
@@ -127,14 +99,15 @@ export default {
   top: 50%;
   left: 50%;
   transform: translate(-50%, 10%);
+  overflow: hidden;
 }
 .main > h1 {
   margin-top: 50px;
   font-family: "NFM", "SCR";
 }
 .baberrage-stage {
-  padding-top: 10px;
-  height: 650px;
+  height: 700px;
+  overflow: hidden;
 }
 .logo {
   font-size: 120px;
